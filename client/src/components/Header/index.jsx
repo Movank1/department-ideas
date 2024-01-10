3071930cd3d018d114d6f2ab586670e28b7efac4
Daniel Zavala <zavaladaniel151@gmail.com>
2024-01-09 20:12:40 -0500

    Update index.jsx
    updated css

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -19,11 +19,11 @@ const Header = () => {
         <div>
           {Auth.loggedIn() ? (
             <>
-              <Link className="btn btn-lg btn-info m-2" to="/me">
+              <Link className="btn btn-sm btn-info m-2" to="/me">
                 {/* Run the getProfile() method to get access to the unencrypted token value in order to retrieve the user's username  */}
                 {Auth.getProfile().authenticatedPerson.username}'s profile
               </Link>
-              <button className="btn btn-lg btn-light m-2" onClick={logout}>
+              <button className="btn btn-sm btn-light m-2" onClick={logout}>
                 Logout
               </button>
             </>
