188db745cc6b4c7d5bccc0f8aa1be5c2470cb67a
Daniel Zavala <zavaladaniel151@gmail.com>
2024-01-09 20:12:28 -0500

    Update index.jsx
    updated CSS

--- client/src/components/Footer/index.jsx
+++ client/src/components/Footer/index.jsx
@@ -4,20 +4,20 @@ const Footer = () => {
   const location = useLocation();
   const navigate = useNavigate();
   return (
-    <footer className="w-100 mt-auto bg-secondary p-4">
-      <div className="container text-center mb-5">
-        {location.pathname !== '/' && (
+    <footer className="bg-primary w-100 mt-auto bg-secondary p-4">
+      <div className="container text-center mb-0">
+        {/* {location.pathname !== '/' && (
           <button
             className="btn btn-dark mb-3"
             onClick={() => navigate(-1)}
           >
             &larr; Go Back
           </button>
-        )}
+        )} */}
         <h4>
-
-          Teamwork; communication are the key of success.
-        </h4>
+        "Success is the sum of small efforts, repeated day-in and day-out." - Robert Collier        </h4>
+      </div>
+      <div>
       </div>
     </footer>
   );
