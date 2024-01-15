96306db33f5dbc88a974ee4b1ae63ff1ba7b9d25
Daniel Zavala <zavaladaniel151@gmail.com>
2024-01-15 18:50:48 -0500

    Update index.jsx

--- client/src/components/Footer/index.jsx
+++ client/src/components/Footer/index.jsx
@@ -5,13 +5,25 @@ const Footer = () => {
   const navigate = useNavigate();
 
   return (
-    <footer className="w-100 mt-auto bg-secondary p-4">
-      < div className="container text-center mb-5">
-        <h4>
-          "Success is the sum of small efforts, repeated day-in and day-out." - Robert Collier
-        </h4>
-      </div>
-    </footer>
+    <footer className="w-100 mt-auto bg-primary p-4">
+      <div role="group" aria-label="1 of 3" aria-roledescription="slide" tabIndex="-1" class="uk-active uk-transition-active">
+
+      <ul class="uk-slideshow-items" aria-live="polite" role="presentation" id="uk-slideshow-17" >
+          <li>
+              <a href='https://github.com/Movank1'>Movank1</a>
+          </li>
+          <li>
+            <a href='https://github.com/Develepor-Dan'>Develepor-Dan</a>
+          </li>
+          <li>
+              <a href='https://github.com/Muisagara'>Muisagara</a>
+          </li>
+      </ul>
+
+<a class="uk-slidenav-large uk-position-center-left uk-position-small uk-hidden-hover" href="#" uk-slidenav-previous="true" uk-slideshow-item="previous"></a>
+<a class="uk-slidenav-large uk-position-center-right uk-position-small uk-hidden-hover" href="#" uk-slidenav-next="true" uk-slideshow-item="next"></a>
+
+</div>    </footer>
   );
 };
 
