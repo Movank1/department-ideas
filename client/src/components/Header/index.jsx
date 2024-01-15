6cf96bee1ff5aae50fe3d54ea82c29ab9078940a
Daniel Zavala <zavaladaniel151@gmail.com>
2024-01-15 13:10:40 -0500

    Update index.jsx

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -7,16 +7,28 @@ const Header = () => {
     event.preventDefault();
     Auth.logout();
   };
+
   return (
-    <header className="bg-primary text-light mb-4 py-3 flex-row align-center">
-      <div className="container flex-row justify-space-between-lg justify-center align-center">
+    <header className="bg-primary text-light mb-4 py-3 flex-row align-center justify-space-around">
+      {/* <div className=" flex-row justify-space-between-lg justify-center align-center "> */}
+        <div><img src="../../icon.png" alt="Company Logo" /></div>
         <div>
           <Link className="text-light" to="/">
-           
-            <h1 class="uk-heading-line uk-text-center"><span>Department Communication App</span></h1>
+            <h1 className=""><span>Dept. Communication</span></h1>
           </Link>
-       
         </div>
+          <button class="uk-button uk-button-default" type="button" uk-toggle="target: #offcanvas-flip">DropDown</button>
+          <div id="offcanvas-flip" uk-offcanvas="flip: true; overlay: true">
+              <div class="uk-offcanvas-bar">
+                  <button class="uk-offcanvas-close" type="button" uk-close="true"></button>
+                  <ul class="uk-nav uk-nav-default">
+                  <li class="uk-active"><a href="/me">My Profile</a></li>
+                  <li class="uk-active"><a href="/">Dashboard</a></li>
+                    <li><a href="/feed">Feed</a></li>
+                    <li><a href="#">Item</a></li>
+                </ul>              
+              </div>
+        </div>        
         <div>
           {Auth.loggedIn() ? (
             <>
@@ -24,20 +36,15 @@ const Header = () => {
                 Logout
               </button>
             </>
-            
           ) : (
             <>
               <Link className="btn btn-lg btn-info m-2" to="/login">
                 Login
               </Link>
-              <Link className="btn btn-lg btn-light m-2" to="/signup">
-                Signup
-              </Link>
             </>
-            
           )}
         </div>
-      </div>
+      {/* </div> */}
     </header>
   );
 };
